68fb629adf710cf6c477d6170ee2755288b97e89
Fatih sarı <32927711+fatihsari17@users.noreply.github.com>
2023-07-17 08:59:52 +0300

    project add push
    homework finish

---
+++ app.js
@@ -0,0 +1,61 @@
+let url = "https://api.openweathermap.org/data/2.5/";
+let key = '6a1016c70d059543ffeab4e84106ef1f';
+
+let displayResult = (result) => {
+    let city = document.querySelector('.city');
+    city.innerText = `${result.name}, ${result.sys.country}`;
+    let temprate = document.querySelector('.temprate');
+    temprate.innerText = `${Math.round(result.main.temp)}°C`;
+    let descriptions = document.querySelector('.description');
+    descriptions.innerText = result.weather[0].description;
+    let ranges = document.querySelector('.range');
+    ranges.innerText = `${Math.round(result.main.temp_min)}°C Sıcaklık / ${Math.round(result.main.temp_max)}°C Hissedilen`;
+    setBackground(result.name);
+};
+let setBackground = (cityName) => {
+    let body = document.querySelector('body');
+    let backgroundUrl = `https://source.unsplash.com/1600x900/?${cityName}`;
+  
+    body.style.backgroundImage = `url('${backgroundUrl}')`;
+  };
+
+let displayError = () => {
+    let errorText = document.querySelector('.error-text');
+    errorText.innerText = 'Yanlış terim girdiniz. Lütfen tekrar deneyin.';
+};
+
+let getResult = (cityName) => {
+    let query = `${url}weather?q=${cityName}&appid=${key}&units=metric&lang=tr`;
+    fetch(query)
+        .then(weather => {
+            if (weather.ok) {
+                return weather.json();
+            } else {
+                throw new Error('Yanlış terim girdiniz.');
+            }
+        })
+        .then(displayResult)
+        .catch(() => {
+            displayError();
+        });
+};
+
+let setQuery = (event) => {
+    if (event.keyCode === 13) {
+        let searchBar = document.getElementById('SearchBar');
+        let searchTerm = searchBar.value;
+
+        // Temizleme işlemi
+        let errorText = document.querySelector('.error-text');
+        errorText.innerText = '';
+
+        if (searchTerm.trim() !== '') {
+            getResult(searchTerm);
+        } else {
+            displayError();
+        }
+    }
+};
+
+let searchBar = document.getElementById('SearchBar');
+searchBar.addEventListener('keypress', setQuery);
